775b987699683c97391932cf04443a8dcc0744c7
martin-kagya <mkagyemang2@st.knust.edu.gh>
2023-12-29 21:32:52 +0000

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Home from './Components/Home'
-import Gallery from './Components/gallery'
+import Gallery from './Components/Gallery'
 import About from './Components/About'
 import Navbar from './Components/Navbar'
 import Services from './Components/Services'
