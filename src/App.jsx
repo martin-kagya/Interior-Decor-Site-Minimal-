6ea4a9f527db87fe6917fa2130d7c2055832d60e
martin-kagya <mkagyemang2@st.knust.edu.gh>
2023-12-30 20:57:16 +0000

    tries

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Home from './Components/Home'
 import Gallery from './Components/Gallery'
 import About from './Components/About'
 import Navbar from './Components/Navbar'
+import { MdKeyboardArrowUp } from "react-icons/md";
 import Services from './Components/Services'
 import Testimonial from './Components/Testimonial'
 import './App.css'
 import Contact from './Components/Contact'
 
+
+
 function App() {
+
+const [visible, setisVisible] = useState(false);
+
+function handleScroll(){
+  const amountScrolled = document.documentElement.scrollTop;
+  setisVisible (amountScrolled > 100)
+}
+
+const scroll = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+useEffect (() => {
+  window.addEventListener("scroll", handleScroll)
+  return () => {
+    window.removeEventListener("scroll", handleScroll)
+  }
+}, [])
   return (
     <>
     <div className='container'>
@@ -19,6 +43,7 @@ function App() {
       <Gallery />
       <Services />
       <Contact />
+      <div className={`scroll-to-top ${setisVisible ? 'visible' : ''}`} onClick={scroll}><MdKeyboardArrowUp size={25}/></div>
     </div>
     </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar(){
         <div className="navbar">
             <Link to="about" smooth={true} duration={200} offset={-50}>about</Link>
             <Link to="gallery" smooth={true} duration={200} offset={-50}>gallery</Link>
-            <Link to="testimonial" smooth={true} duration={200} offset={-50}>contact</Link>
             <Link to="services" smooth={true} duration={200} offset={-50}>services</Link>
+            <Link to="contact" smooth={true} duration={200} offset={-50}>contact</Link>
         </div>
     );
 }
